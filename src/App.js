e8326efcc9f16f3ff2a796ccec02b175a159c729
Jaylers <jay.cm@traditionasia.com>
2019-08-19 16:17:52 +0700

    add first project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './asset/logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <title>React Sample</title>
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to React
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://jaylers.com"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noreferrer">
+          Home
         </a>
       </header>
     </div>
