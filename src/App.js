820ec28c75a8189861b438ef731ca2d4bb5e3586
Jaylers <jay.cm@traditionasia.com>
2019-08-20 16:06:29 +0700

    move out welcome page

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
 import React from 'react';
-import logo from './asset/logo.svg';
 import './css/App.css';
-import { Button } from 'reactstrap';
 import Header from "./component/Header";
-import { Container, Row, Col } from 'reactstrap';
+import Welcome from "./component/Welcome";
 
 function App() {
   return (
     <div className="App">
-
-      <Header/>
-
-        <div className="App">
-            <Container>
-                <Row>
-                    <Col xs="2">
-
-                    </Col>
-                    <Col xs="auto">.col-auto - variable width content</Col>
-                </Row>
-            </Container>
-            <div className="m-lg-1 m-sm-1 m-md-1">
-                HHHHH
-            </div>
-
-            <div>
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-
-                    <Button color="primary">primary</Button>{' '}
-
-                </header>
-            </div>
-        </div>
-
+        <Header/>
+        <Welcome/>
     </div>
   );
 }
---
+++ src/component/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from "../asset/logo.svg";
+
+export default class Welcome extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+            </div>
+        );
+    }
+}
