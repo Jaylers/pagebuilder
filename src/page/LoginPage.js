e8d7814d75de6b398f24a874ced422375cfc441a
Jaylers <jay.cm@traditionasia.com>
2019-08-29 17:39:18 +0700

    connect to api with text/plan

--- src/page/LoginPage.js
+++ src/page/LoginPage.js
@@ -11,37 +11,33 @@ import * as Validator from "../service/Validator";
 export default function LoginPage() {
     const cookies = new Cookies();
     const lastUser = cookies.get(Application.LAST_USER);
-    const [username, typingUsername] = useState(lastUser? lastUser.username : "admin");
+    const [username, typingUsername] = useState(lastUser ? lastUser.username : "admin");
     const [password, typingPassword] = useState("123456");
     verifyLogin();
-    function  verifyLogin() {
-        if (cookies.get(Application.USER)){
+
+    function verifyLogin() {
+        if (cookies.get(Application.USER)) {
             window.location.href = "/";
         } else {
             cookies.remove(Application.USER);
         }
     }
 
-    function AttemptToLogin(){
-        if (!Validator.isValidUsername(username)){
+    function AttemptToLogin() {
+        if (!Validator.isValidUsername(username)) {
             displayError("Required", "Username is required!");
-        } else if (!password){
+        } else if (!password) {
             displayError("Required", "Password is required!");
-        } else if (username && password){
-
+        } else if (username && password) {
             const request = {
-                "username": username.toUpperCase(),
-                "password" : password
+                username: username,
+                password: password
             };
 
             let config = {
-                "Access-Control-Request-Headers" : "Content-Type",
-                "Content-Type" : "application/json",
-                Accept : "application/json",
-                withCredentials: true,
+                headers : { "Content-Type" : "text/plain" }
             };
 
-            Axios.create(config);
             Axios.post(ServiceApi.POST_LOGIN, request, config)
                 .then(function (response) {
                     let data = response.data;
@@ -62,7 +58,7 @@ export default function LoginPage() {
     }
 
     function displayError(title, message) {
-        console.log(title + " <> "+ message);
+        console.log(title + " <> " + message);
     }
 
     function handleKeyDown(key) {
@@ -72,27 +68,27 @@ export default function LoginPage() {
     }
 
     return (
-        <>
-            <div> </div>
-            <div className="App login-container bg-url">
-                <a href={Path.index} className="login-back-home">home</a>
-                <form className="login-form">
-                    <img src={Code} className="login-brand" alt={"logo"}/> <br/>
-                    <input id="username" type="text" autoComplete="username" className="login-input" placeholder="Username" value={username}
-                           onChange={ event => typingUsername(event.target.value)} /> <br/> <br/>
-                    <input id="password" type="password" autoComplete="current-password" className="login-input" placeholder="Password" value={password}
-                           onChange={ event => typingPassword(event.target.value)}
-                           onKeyPress={ event => handleKeyDown(event.key) }/> <br/>
+        <div className="App login-container bg-url">
+            <a href={Path.index} className="login-back-home">home</a>
+            <form className="login-form">
+                <img src={Code} className="login-brand" alt={"logo"}/> <br/>
+                <input id="username" type="text" autoComplete="username" className="login-input"
+                       placeholder="Username" value={username}
+                       onChange={event => typingUsername(event.target.value)}/> <br/> <br/>
+                <input id="password" type="password" autoComplete="current-password" className="login-input"
+                       placeholder="Password" value={password}
+                       onChange={event => typingPassword(event.target.value)}
+                       onKeyPress={event => handleKeyDown(event.key)}/> <br/>
 
-                    <Button onClick={ () => new AttemptToLogin() } className="login-btn">
-                        Sign in
-                    </Button>
-                    <br/>
+                <Button onClick={() => new AttemptToLogin()} className="login-btn">
+                    Sign in
+                </Button>
+                <br/>
 
-                    <a href={Path.register} className="login-href">Sign up</a> | <a href={Path.forget} className="login-href">Forget?</a>
-                </form>
+                <a href={Path.register} className="login-href">Sign up</a> | <a href={Path.forget}
+                                                                                className="login-href">Forget?</a>
+            </form>
 
-            </div>
-        </>
+        </div>
     );
 }
